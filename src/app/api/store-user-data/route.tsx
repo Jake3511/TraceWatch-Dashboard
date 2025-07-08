999c80efd9a078514dd528365fd1f03ac40cc27c
Jake Shick <jake_shick@student.uml.edu>
2025-07-08 13:41:35 -0400

    Added api verification and created folder system for metrics.  This will be used later when frontend code will make requests to backend

--- src/app/api/store-user-data/route.tsx
+++ src/app/api/store-user-data/route.tsx
@@ -1,17 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 import { pool } from "@/lib/db";
 import bcrypt from "bcrypt";
-import { randomBytes } from "crypto"; 
 
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
         const { metric, apiKey, data } = body;
         
-        const result = pool.query('SELECT api_key_hash FROM users');
-        
+        if (apiKey != "") {
+            return NextResponse.json({ error: 'API key is required' }, { status: 401 });
+        }
 
-        return NextResponse.json({ message: 'Data received successfully' }, { status: 200 });
+        const result = await pool.query('SELECT api_key_hash FROM users');
+
+        const valid = result.rows.some(row => 
+            bcrypt.compareSync(apiKey, row.api_key_hash)
+        );
+
+        if (!valid) {
+            return NextResponse.json({ error: 'No valid API key was entered' }, { status: 401 });
+        }
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/metrics/${metric}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data })
+        });
+
+        const responseData = await res.json();
+
+        return NextResponse.json({
+            message: `Forwarded data to /api/${metric}`,
+            status: res.status,
+            response: responseData,
+        }, { status: res.status});
 
     } catch (error) {
         console.error("Error parsing JSON body:", error);
