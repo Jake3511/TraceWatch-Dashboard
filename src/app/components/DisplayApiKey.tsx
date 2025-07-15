0f309bd89b9a3564a114173972f3e568d8be1a8f
Jake Shick <jake_shick@student.uml.edu>
2025-07-15 14:50:12 -0400

    Trying to fix deployment issues

--- src/app/components/DisplayApiKey.tsx
+++ src/app/components/DisplayApiKey.tsx
@@ -1,15 +1,18 @@
 'use client'
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+// import React, {useState, useEffect} from 'react'
 
 const DisplayApiKey: React.FC = () => {
-    const [apiKey, setApiKey] = useState<string | null>(null);
+//     const [apiKey, setApiKey] = useState<string | null>(null);
 
-    const handleApiKey = async () => {
-        const res = await fetch('/api/auth/')
-    }
+//     const handleApiKey = async () => {
+//         const res = await fetch('/api/auth/')
+//     }
     
     return (
     <>
         <h1>To return to login page, please copy and paste you're Api key to a secure location.</h1>
     </>
 )}
+
+export default DisplayApiKey;
